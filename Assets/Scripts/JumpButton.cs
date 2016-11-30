8044dc48ec54882c290eef676ad98000b1b93be8
Deinyefa Eporwei <df.eporwei@gmail.com>
2016-11-30 12:25:37 -0500

    feat: jump animation and lose condition

---
+++ Assets/Scripts/JumpButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class JumpButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	public void OnPointerDown (PointerEventData data) {
+		if (PlayerJump.instance != null) {
+			PlayerJump.instance.SetPower (true); 
+		}
+	}
+
+	public void OnPointerUp (PointerEventData data) {
+		if (PlayerJump.instance != null) {
+			PlayerJump.instance.SetPower (false); 
+		}
+	}
+
+}
